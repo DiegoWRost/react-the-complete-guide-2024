5c01a4f16ac412f1fe630fe8173a180298097717
DiegoWR <diego.wr@gmail.com>
2024-01-30 20:28:58 -0700

    03-38-Creating and using first component

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import ReactDOM from "react-dom/client";
 
-import App from "./App.jsx";
-import "./index.css";
+import App from "./03-react-essentials/App"
+import "./03-react-essentials/index.css";
 
 const entryPoint = document.getElementById("root");
 ReactDOM.createRoot(entryPoint).render(<App />);
