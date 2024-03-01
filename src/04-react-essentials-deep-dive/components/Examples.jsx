591b879c0ba9a101c31084749d662eb08c777d15
DiegoWR <diego.wr@gmail.com>
2024-02-29 17:33:58 -0700

    04-66. Working with Multiple JSX Slots

--- src/04-react-essentials-deep-dive/components/Examples.jsx
+++ src/04-react-essentials-deep-dive/components/Examples.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 import TabButton from "./TabButton";
 import Section from "./Section";
+import Tabs from "./Tabs";
 import { EXAMPLES } from "../data";
 
 const menuButtons = ["Components", "JSX", "Props", "State"];
@@ -15,27 +16,32 @@ export default function Examples() {
 
   return (
     <Section title="Examples" id="examples">
-      <menu>
-        {menuButtons.map((buttonName) => (
-          <TabButton
-            isSelected={selectedTopic === buttonName.toLowerCase()}
-            onClick={() => handleSelect(buttonName.toLowerCase())}
-          >
-            {buttonName}
-          </TabButton>
-        ))}
-      </menu>
-      {!selectedTopic && <p>Please select a topic</p>}
+      <Tabs
+        buttons={
+          <>
+            {menuButtons.map((buttonName) => (
+              <TabButton
+                isSelected={selectedTopic === buttonName.toLowerCase()}
+                onClick={() => handleSelect(buttonName.toLowerCase())}
+              >
+                {buttonName}
+              </TabButton>
+            ))}
+          </>
+        }
+      >
+        {!selectedTopic && <p>Please select a topic</p>}
 
-      {selectedTopic && (
-        <div id="tab-content">
-          <h3>{EXAMPLES[selectedTopic].title}</h3>
-          <p>{EXAMPLES[selectedTopic].description}</p>
-          <pre>
-            <code>{EXAMPLES[selectedTopic].code}</code>
-          </pre>
-        </div>
-      )}
+        {selectedTopic && (
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>{EXAMPLES[selectedTopic].code}</code>
+            </pre>
+          </div>
+        )}
+      </Tabs>
     </Section>
   );
 }
